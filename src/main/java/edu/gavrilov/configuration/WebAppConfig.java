6b8cb8f016fa26573519afcdaf2c20e778a7af58
GavrilovSV <gavrilovsv_95@mail.ru>
2018-10-16 23:56:41 +0300

    Icon added

--- src/main/java/edu/gavrilov/configuration/WebAppConfig.java
+++ src/main/java/edu/gavrilov/configuration/WebAppConfig.java
@@ -25,7 +25,7 @@ public class WebAppConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**")
-                .addResourceLocations("/resources");
+                .addResourceLocations("/resources/");
     }
 
     @Bean
